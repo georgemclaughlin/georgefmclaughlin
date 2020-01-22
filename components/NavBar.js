7e73a7139dd70779726aa390d9562d970dd5a208
georgemclaughlin <george2gfm@gmail.com>
2020-01-22 17:28:15 -0600

    initial commit

---
+++ components/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbor from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const NavBar = () => {
+    return(
+        <div>
+            <AppBar position="static">
+                <Toolbor>
+                    <Typography variant="title" color="inherit">
+                        George F McLaughlin
+                    </Typography>
+                </Toolbor>
+            </AppBar>
+        </div>
+    )
+}
+
+export default NavBar;
---
+++ pages/Index.js
@@ -0,0 +1,11 @@
+import NavBar from '../components/navbar'
+
+const Index = () => {
+  return (
+      <div>
+        <NavBar />
+      </div>
+    )
+}
+
+export default Index
